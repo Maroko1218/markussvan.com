aa6fb01771541fb22ce37cac3ec224bf61d435e3
Markus Svan <markus.svhn@gmail.com>
2025-03-10 20:45:21 +0100

    Simplified chown of run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/Tabor/websites/markussvan.com/ &&\
+chown -R root:root .
 hugo --cleanDestinationDir &&\
 rm -rf /srv/http/markussvan.com/* &&\
 cp -r public/* /srv/http/markussvan.com/ &&\
-chown -R root:root /srv/http/markussvan.com/*
+chown -R Tabor:Tabor .
