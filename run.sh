25b5f672eec30fcf4c5940012861a998a9c4963e
Markus Svan <markus.svhn@gmail.com>
2025-03-11 17:44:50 +0100

    Much cleaner build script

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd /home/Tabor/websites/markussvan.com/ &&\
+cd /home/Tabor/websites/markussvan.com/
 chown -R root:root .
-hugo --cleanDestinationDir &&\
-rm -rf /srv/http/markussvan.com/* &&\
-cp -r public/* /srv/http/markussvan.com/ &&\
+hugo --cleanDestinationDir -d /srv/http/markussvan.com
 chown -R Tabor:Tabor .
